b71ed33fabdfd83752b4fd473bdad40afca2f4ee
juhyeonjong <vkdlfjfxm2@naver.com>
2024-01-25 22:20:36 +0900

    프로토 타이핑 메인페이지
    -아이콘 수정 필요

--- Documents/6. 프로토타입/base.js
+++ Documents/6. 프로토타입/base.js
@@ -4,14 +4,14 @@ let currentIdx = 0; //현재 슬라이드 index
 const slideCount = slideImg.length; // 슬라이드 개수
 const prev = document.querySelector('.prev'); //이전 버튼
 const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 300; //한개의 슬라이드 넓이
+const slideWidth = 400; //한개의 슬라이드 넓이
 const slideMargin = 100; //슬라이드간의 margin 값
 
 //전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
 function moveSlide(num) {
-  slides.style.left = -num * 400 + 'px';
+  slides.style.left = -num * 500 + 'px';
   currentIdx = num;
 }
 
