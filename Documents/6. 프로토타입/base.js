68aa9e655d6285287b94fea72cf7c93becc58a1b
juhyeonjong <vkdlfjfxm2@naver.com>
2024-01-26 15:16:46 +0900

    프로토타이핑 메인페이지 디테일 작업
    -디테일 작업 완
    -불필요한 파일 삭제
    -아이콘 추가

--- Documents/6. 프로토타입/base.js
+++ Documents/6. 프로토타입/base.js
@@ -1,34 +1,11 @@
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 400; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
-
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
-
-function moveSlide(num) {
-  slides.style.left = -num * 500 + 'px';
-  currentIdx = num;
-}
-
-prev.addEventListener('click', function () {
-  /*첫 번째 슬라이드로 표시 됐을때는 
-  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-  currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-  if (currentIdx !== 0) moveSlide(currentIdx - 1);
-});
-
-next.addEventListener('click', function () {
-  /* 마지막 슬라이드로 표시 됐을때는 
-  다음 버튼 눌러도 아무런 반응 없게 하기 위해
-  currentIdx !==slideCount - 1 일때만 
-  moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
-  }
+$(document).ready(function () {
+  $('.bxslider').bxSlider({
+      auto: false, // 자동으로 애니메이션 시작
+      speed: 500,  // 애니메이션 속도
+      pause: 5000,  // 애니메이션 유지 시간 (1000은 1초)
+      mode: 'horizontal', // 슬라이드 모드 ('fade', 'horizontal', 'vertical' 이 있음)
+      autoControls: true, // 시작 및 중지버튼 보여짐
+      pager: true, // 페이지 표시 보여짐
+      captions: true, // 이미지 위에 텍스트를 넣을 수 있음
+  });
 });
