a78f3081267210a769096ea424c5e3eb361a72e5
JeonWooSung <neblasta85@gmail.com>
2024-02-13 13:12:57 +0900

    Base62 로직 추가
    - HashMaker에 encoding, decoding 추가

--- Project/src/main/java/ezen/util/HashMaker.java
+++ Project/src/main/java/ezen/util/HashMaker.java
@@ -4,6 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashMaker {
+	private static Base62 base62 = new Base62();
 	
     public static String sha256(String msg) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -19,4 +20,40 @@ public class HashMaker {
         }
         return builder.toString();
     }
+    
+    
+    public static String Base62Encoding(long param) {
+    	return base62.encoding(param);
+    }
+    
+    public static long Base62Decoding(String param) {
+    	return base62.decoding(param);
+    }
+    
+    
+}
+
+
+class Base62 {
+	final int RADIX = 62;
+	final String CODEC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	
+	public String encoding(long param) {
+		StringBuffer sb = new StringBuffer();
+		while(param > 0) {
+			sb.append(CODEC.charAt((int) (param % RADIX)));
+			param /= RADIX;
+		}
+		return sb.toString();
+	}
+	
+	public long decoding(String param) {
+		long sum = 0;
+		long power = 1;
+		for (int i = 0; i < param.length(); i++) {
+			sum += CODEC.indexOf(param.charAt(i)) * power;
+			power *= RADIX;
+		}
+		return sum;
+	}
 }
